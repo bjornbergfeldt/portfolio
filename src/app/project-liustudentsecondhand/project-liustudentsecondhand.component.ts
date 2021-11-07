305a799a428f8a01fcdb19d8b1b2afaa0b03d347
unknown <bjobe613@student.liu.se>
2021-11-07 21:28:02 +0100

    Change og tags to constructor

--- src/app/project-liustudentsecondhand/project-liustudentsecondhand.component.ts
+++ src/app/project-liustudentsecondhand/project-liustudentsecondhand.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Meta } from '@angular/platform-browser';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-project-liustudentsecondhand',
@@ -8,17 +9,20 @@ import { Meta } from '@angular/platform-browser';
 })
 export class ProjectLiustudentsecondhandComponent implements OnInit {
 
-  constructor(private metaTagService: Meta) { }
+  constructor(private metaTagService: Meta, private titleService: Title) { 
 
-  ngOnInit(): void {
+    this.titleService.setTitle("LiU Student Secondhand");
 
     this.metaTagService.addTags([
       {property: 'og:title', content: 'LiU Student Secondhand'},
       {property: 'og:type', content: 'website'},
+      {property: 'og:description', content: 'Project leader and developer for a new webapplication for the student association "LiU Student Secondhand" at Linköping University. The developers for this was me together with Filip Brunander, Isak Almquist and Alexander Bois.'},
       {property: 'og:url', content: 'https://bjornbergfeldt.github.io/portfolio/liustudentsecondhand'},
-      {property: 'og:image', content: 'https://live.staticflickr.com/65535/50695970536_d494012b59.jpg'}
+      {property: 'og:image', content: 'https://live.staticflickr.com/65535/51662893509_f24b2772dd.jpg'}
     ]);
 
   }
+
+  ngOnInit(): void {}
 
 }
